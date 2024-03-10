c6b735df2aae5e6fe980af03e7d67e2d0706a17b
Himanshu <yadavhimanshu51@gmail.com>
2024-03-10 22:27:22 +0530

    vite changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import Home from "./modules/Home";
-import Apply from "./modules/applicant/Apply";
-import JobListings from "./modules/applicant/JobListings";
-import ATSPage from "./modules/ats/ATSPage";
-import AddPosition from "./modules/recruiter/AddPosition";
-import JobPosts from "./modules/recruiter/JobPosts";
-import Footer from "./modules/shared/Footer";
-import Header from "./modules/shared/Header";
+// import Apply from "./modules/applicant/Apply";
+const Apply = lazy(() => import("./modules/applicant/Apply"));
+// import JobListings from "./modules/applicant/JobListings";
+const JobListings = lazy(() => import("./modules/applicant/JobListings"));
+const ATSPage = lazy(() => import("./modules/ats/ATSPage"));
+// import ATSPage from "./modules/ats/ATSPage";
+const AddPosition = lazy(() => import("./modules/recruiter/AddPosition"));
+// import AddPosition from "./modules/recruiter/AddPosition";
+const JobPosts = lazy(() => import("./modules/recruiter/JobPosts"));
+// import JobPosts from "./modules/recruiter/JobPosts";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Outlet, Link } from "react-router-dom";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -16,27 +19,51 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: (
+        <Suspense>
+          <Home />
+        </Suspense>
+      ),
       children: [
         {
           path: "/",
-          element: <JobListings />,
+          element: (
+            <Suspense>
+              <JobListings />,
+            </Suspense>
+          ),
         },
         {
           path: "/add-position",
-          element: <AddPosition />,
+          element: (
+            <Suspense>
+              <AddPosition />,
+            </Suspense>
+          ),
         },
         {
           path: "/job-posts",
-          element: <JobPosts />,
+          element: (
+            <Suspense>
+              <JobPosts />,
+            </Suspense>
+          ),
         },
         {
           path: "/ats/:jobid",
-          element: <ATSPage />,
+          element: (
+            <Suspense>
+              <ATSPage />,
+            </Suspense>
+          ),
         },
         {
           path: "/:jobtitle/apply/:jobid",
-          element: <Apply />,
+          element: (
+            <Suspense>
+              <Apply />,
+            </Suspense>
+          ),
         },
       ],
     },
